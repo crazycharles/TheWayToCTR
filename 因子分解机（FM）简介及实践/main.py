0ce83723a04050534a3d31eefafce7d3c83c5516
Chaojie An <anchaojie@stu.xmu.edu.cn>
2020-05-30 10:59:27 +0800

    update

--- 因子分解机（FM）简介及实践/main.py
+++ 因子分解机（FM）简介及实践/main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu May 28 09:45:19 2020
-
+python 3.6
+tensorflow 1.10.0
 @author: an
 """
 
@@ -9,13 +10,16 @@ from fm import FM
 
 from sklearn.datasets import load_boston
 import tensorflow as tf
+from sklearn import preprocessing
+
 x, y = load_boston(return_X_y=True)
-learning_rate = 1e-2
+x = preprocessing.scale(x)
+
+learning_rate = 1e-3
 batch_size = 16
 epoch = 1000
 
 tf.reset_default_graph()
-
 #initial
 regressor = FM(x,y,learning_rate = learning_rate, batch_size = batch_size, epoch = epoch)
 #train
